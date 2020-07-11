fbe9b04bdf3ca045f9c7a16d290f6b68650bb8e5
John Ide <ide.johnc@gmail.com>
2020-07-10 20:54:30 -0400

    Expanded on errors

--- headquarters/errors.go
+++ headquarters/errors.go
@@ -2,16 +2,32 @@ package main
 
 import "fmt"
 
-type deliveryNotFoundError struct {
+type notFoundError struct {
 	Code    int    `json:"code"`
 	Message string `json:"message"`
 }
 
-func (e deliveryNotFoundError) Error() string {
+func (e notFoundError) Error() string {
 	return fmt.Sprintf("error [%d]: %s", e.Code, e.Message)
 }
 
-func (e deliveryNotFoundError) Extensions() map[string]interface{} {
+func (e notFoundError) Extensions() map[string]interface{} {
+	return map[string]interface{}{
+		"code":    e.Code,
+		"message": e.Message,
+	}
+}
+
+type internalError struct {
+	Code    int    `json:"code"`
+	Message string `json:"message"`
+}
+
+func (e internalError) Error() string {
+	return fmt.Sprintf("error [%d]: %s", e.Code, e.Message)
+}
+
+func (e internalError) Extensions() map[string]interface{} {
 	return map[string]interface{}{
 		"code":    e.Code,
 		"message": e.Message,
